c52446023072859110bead2ce5897e606359cf89
Henry Cheng <henry@bluecats.com>
2022-01-24 16:37:54 +1100

    update

--- Package.swift
+++ Package.swift
@@ -20,23 +20,9 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-//        .target(
-//            name: "BlueCatsSDK-Swift",
-//            dependencies: ["BlueCatsSDK"],
-//            path: "Sources/bluecats-ios-sdk-swift",
-//            linkerSettings: [
-//                //Frameworks
-//
-//            ]
-//        ),
         .binaryTarget(
             name: "BlueCatsSDK", //binary target name must be same as the module name
             path: "BlueCatsSDK.xcframework"
         )
-//        .testTarget(
-//            name: "bluecats-ios-sdk-swiftTests",
-//            dependencies: ["bluecats-ios-sdk-swift"]),
     ]
 )
